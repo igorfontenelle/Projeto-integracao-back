3785300902d3e4908b597aac9a3c9edb0e24d6c5
Igor Fontenele <igorfelias09@gmail.com>
2021-07-07 13:24:07 -0300

    #DELETED - Enum Ares.java - Enum Cronos
    #ADDED
    - create file MeasurerJpaDAO.java
    - create file MeasurerModel.java
    - create file persistence.xml

---
+++ src/main/java/br/com/eletra/dao/MeasurerJpaDAO.java
@@ -0,0 +1,87 @@
+package br.com.eletra.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.eletra.model.MeasurerModel;
+
+public class MeasurerJpaDAO {
+	private static MeasurerJpaDAO instance;
+	protected EntityManager entityManager;
+	
+	public static MeasurerJpaDAO getInstance() {
+		if (instance == null) {
+			instance = new MeasurerJpaDAO();
+		}
+		
+		return instance;
+	}
+	
+	private MeasurerJpaDAO() {
+		entityManager = getEntityManager();
+	}
+	
+	private EntityManager getEntityManager() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("medidores-persistence");
+		if (entityManager == null) {
+			entityManager = factory.createEntityManager();
+		}
+		return entityManager;
+	}
+	
+	public MeasurerModel getById(final int id) {
+	      return entityManager.find(MeasurerModel.class, id);
+	}
+	
+	@SuppressWarnings("unchecked")
+    public List<MeasurerModel> findAll() {
+      //return entityManager.createQuery("FROM " + Cliente.class.getName()).getResultList();
+    	return entityManager.createQuery("select m FROM " + MeasurerModel.class.getName() + " m").getResultList();
+    }
+	
+	public void persist(MeasurerModel measurer) {
+	      try {
+	         entityManager.getTransaction().begin();
+	         entityManager.persist(measurer);
+	         entityManager.getTransaction().commit();
+	      } catch (Exception ex) {
+	         ex.printStackTrace();
+	         entityManager.getTransaction().rollback();
+	      }
+	}
+	
+	public void merge(MeasurerModel measurer) {
+		try {
+			entityManager.getTransaction().begin();
+			entityManager.merge(measurer);
+			entityManager.getTransaction().commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			entityManager.getTransaction().rollback();
+		}
+	}
+	
+	public void remove(MeasurerModel measurer) {
+	      try {
+	         entityManager.getTransaction().begin();
+	         measurer = entityManager.find(MeasurerModel.class, measurer.getId());
+	         entityManager.remove(measurer);
+	         entityManager.getTransaction().commit();
+	      } catch (Exception ex) {
+	         ex.printStackTrace();
+	         entityManager.getTransaction().rollback();
+	      }
+	}
+	
+	public void removeById(final int id) {
+	      try {
+	         MeasurerModel measurer = getById(id);
+	         remove(measurer);
+	      } catch (Exception ex) {
+	         ex.printStackTrace();
+	      }
+	}
+}
---
+++ src/main/java/br/com/eletra/model/MeasurerModel.java
@@ -0,0 +1,76 @@
+package br.com.eletra.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="measurermodel")
+public class MeasurerModel implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String line;
+	private String category;
+	private String model;
+	
+	public MeasurerModel() {
+		
+	}
+
+	public MeasurerModel(Integer id, String line, String category, String model) {
+		super();
+		this.id = id;
+		this.line = line;
+		this.category = category;
+		this.model = model;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLine() {
+		return line;
+	}
+
+	public void setLine(String line) {
+		this.line = line;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	@Override
+	public String toString() {
+		return "MeasurerModel [id=" + id + ", line=" + line + ", category=" + category + ", model=" + model + "]";
+	}
+	
+	
+}
