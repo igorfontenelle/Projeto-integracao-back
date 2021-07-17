6cb294d75630586eeee24571fa9ceac5c4cc859f
Igor Fontenele <igorfelias09@gmail.com>
2021-07-17 12:30:41 -0300

    ###update

--- src/main/java/com/measurer/apirest/ApirestMeasurerApplication.java
+++ src/main/java/com/measurer/apirest/ApirestMeasurerApplication.java
@@ -3,6 +3,7 @@ package com.measurer.apirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Apirest
 @SpringBootApplication
 public class ApirestMeasurerApplication {
 
